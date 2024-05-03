9922ddaa3de029c6b718318a6142c08139d3d6ff
hirendranath <contact.hirendranath@gmail.com>
2024-05-03 11:52:44 +0600

    take a input in C

---
+++ Semester 1/Introduction to C/week 1/module 1/output.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    // #1
+    // int hiren, mou;
+    // char c;
+    // float f;
+    // scanf("%d %c %f", &hiren, &c, &f);
+    // printf("%d %c %0.2f", hiren, c, f);
+    
+    // #2
+    int a, b;
+    // scanf("%d%% %d%%", &a, &b);
+    // printf("%d%% %d%%", a, b);
+
+    scanf("%da %db", &a, &b);
+    printf("%da %db", a, b);
+
+
+}
