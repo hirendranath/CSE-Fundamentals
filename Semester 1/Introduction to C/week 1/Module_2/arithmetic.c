86020eea4fc3be87eb6fe67cba2943488b16e0a4
hirendranath <contact.hirendranath@gmail.com>
2024-05-03 17:16:32 +0600

    arithmetic done

---
+++ Semester 1/Introduction to C/week 1/Module_2/arithmetic.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a=3, b=20;
+    // int sum=a+b;
+    // printf("%d", sum);
+
+    // int min=a-b;
+    // printf("%d", min);
+
+    // int mlt=a*b;
+    // printf("%d", mlt);
+
+    // float div=b*1.0/a;
+    // printf("%f", div);
+
+    int mod=b%a;
+    printf("%d", mod);
+
+    return 0;
+}
