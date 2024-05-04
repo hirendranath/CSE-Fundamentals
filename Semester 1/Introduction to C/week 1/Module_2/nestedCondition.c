38580014351ff513bff3b598d2d43ee36b7daf16
hirendranath <contact.hirendranath@gmail.com>
2024-05-05 02:53:53 +0600

    nested if else done with different example

---
+++ Semester 1/Introduction to C/week 1/Module_2/nestedCondition.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if(tk>=10000)
+    {
+        printf("ami UK jabo\n");
+
+        if (tk>=15000)
+        {
+                printf("Bou soho UK jabp\n");
+                }
+            else
+            {
+                printf("bouka neta parbo na\n");
+            }
+        }else{
+            printf("Dubai jabo\n");
+        }
+
+    return 0;
+    
+}
